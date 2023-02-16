98bcc1ff0a47a61428cec854e2d3e87c325d29b7
nageswararao32 <111236990+nageswararao32@users.noreply.github.com>
2023-02-16 13:02:07 +0530

    Create databaseConnection.php

---
+++ databaseConnection.php
@@ -0,0 +1,11 @@
+<?php 
+$servername='localhost';
+$username='root';
+$password='';
+$dbname = "hospital_registration";
+$conn=mysqli_connect($servername,$username,$password,"$dbname");
+if(!$conn){
+   die('Could not Connect My Sql:' .mysql_error());
+}
+
+?>
